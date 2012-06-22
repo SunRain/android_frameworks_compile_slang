5d196a4231f38a054c97b2b790ad7c4d6cc22d09
SunRain <5908183@163.com>
2012-06-22 19:28:21 +0800

    oops
    Change-Id: I4e7ca5f90e418052ede67d44c6cc2e418bc16c29

--- slang_rs_export_foreach.cpp
+++ slang_rs_export_foreach.cpp
@@ -246,10 +246,7 @@ RSExportForEach *RSExportForEach::Create(RSContext *Context,
                                     clang::SourceLocation(),
                                     &Ctx.Idents.get(Id));
 
-<<<<<<< HEAD
       //llvm::StringRef ParamName = PVD->getName();
-=======
->>>>>>> github/ics
       clang::FieldDecl *FD =
           clang::FieldDecl::Create(Ctx,
                                    RD,
